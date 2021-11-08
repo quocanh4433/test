b345102f495266baf6480da30e9cdd7e8a80f468
anhhq <anhhq@basebs.com>
2021-11-08 11:59:06 +0700

    first

---
+++ index.js
@@ -0,0 +1,11 @@
+let api_body = {
+    "fld_contactname_27412890": "biz_infor_policyholder_name",
+    "fld_contactnumber_9873757": "biz_infor_phone_number"
+}
+
+for (let key in api_body) {
+    let value = api_body[key]
+    console.log(key, ':', value)
+}
+
+console.log('Chuyển CVTV xử lý\t')
