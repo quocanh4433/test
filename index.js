9336aa566cb24194e0147a5eed9d746b38baf22f
anhhq <anhhq@basebs.com>
2021-11-08 12:01:29 +0700

    first b

--- index.js
+++ index.js
@@ -9,3 +9,51 @@ for (let key in api_body) {
 }
 
 console.log('Chuyển CVTV xử lý\t')
+
+let api_body = {
+    "fld_contactname_27412890": "biz_infor_policyholder_name",
+    "fld_contactnumber_9873757": "biz_infor_phone_number"
+}
+
+for (let key in api_body) {
+    let value = api_body[key]
+    console.log(key, ':', value)
+}
+
+console.log('Chuyển CVTV xử lý\t')
+
+let api_body = {
+    "fld_contactname_27412890": "biz_infor_policyholder_name",
+    "fld_contactnumber_9873757": "biz_infor_phone_number"
+}
+
+for (let key in api_body) {
+    let value = api_body[key]
+    console.log(key, ':', value)
+}
+
+console.log('Chuyển CVTV xử lý\t')
+
+let api_body = {
+    "fld_contactname_27412890": "biz_infor_policyholder_name",
+    "fld_contactnumber_9873757": "biz_infor_phone_number"
+}
+
+for (let key in api_body) {
+    let value = api_body[key]
+    console.log(key, ':', value)
+}
+
+console.log('Chuyển CVTV xử lý\t')
+
+let api_body = {
+    "fld_contactname_27412890": "biz_infor_policyholder_name",
+    "fld_contactnumber_9873757": "biz_infor_phone_number"
+}
+
+for (let key in api_body) {
+    let value = api_body[key]
+    console.log(key, ':', value)
+}
+
+console.log('Chuyển CVTV xử lý\t')
